0f1f4ab1b104e937eb446afb7b22105cf462b374
uysolanki <uysolanki@gmail.com>
2025-07-15 11:40:18 +0530

    javascript day 5

---
+++ javascriptsessions/day14.js
@@ -0,0 +1,9 @@
+const collegeNameHeadingElement=document.getElementById('heading1')
+collegeNameHeadingElement.innerHTML="KIIT"
+
+collegeNameHeadingElement.style.backgroundColor="blue"
+collegeNameHeadingElement.style.color="white"
+
+collegeNameHeadingElement.style.setProperty("background-color","rgb(114, 176, 26)")
+
+collegeNameHeadingElement.classList.add("college-title")
