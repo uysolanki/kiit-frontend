eed220e6a4272b2dc16edd35f727a82d60eb5833
uysolanki <uysolanki@gmail.com>
2025-07-24 10:22:33 +0530

    react documents

--- reactsessions/react-project1/src/components/SingleProduct.jsx
+++ reactsessions/react-project1/src/components/SingleProduct.jsx
@@ -1,36 +1,32 @@
 import React, { useEffect, useState } from 'react'
 import { useParams } from 'react-router-dom';
+import axios from 'axios';
 import ProductCard from './ProductCard';
 
 const SingleProduct = () => {
+  const { id } = useParams();
+  const [product, setProduct] = useState(null); // null instead of undefined
 
-    const { id } = useParams();
-    const [product,setProduct]=useState()
-     useEffect(
-    ()=>{
-        loadDataFromFakeStoreServer()
-    },[]    //dependency Array    //[] execute the code within the useeffect only once at component load time
-  )                               //[count,count1,product] execute the code within the useeffect everytime any of the state mention is modified
-                                  //, dont provide the dependency array, execute the code within the useeffect everytime any of the state is modified
-  async function loadDataFromFakeStoreServer()
-  {
-    try{
-    const rawData= await axios.get(`https://fakestoreapi.com/products/${id}`)
-    console.log(rawData)
-    setProduct(rawData.data)
-    console.log(product)
-    }
-    catch(error)
-    {
-      console.log(error)
+  useEffect(() => {
+    loadDataFromFakeStoreServer();
+  }, []);
+
+  async function loadDataFromFakeStoreServer() {
+    try {
+      const rawData = await axios.get(`https://fakestoreapi.com/products/${id}`);
+      setProduct(rawData.data);
+    } catch (error) {
+      console.log(error);
     }
   }
 
-  return ( 
+  return (
     <>
-         <ProductCard product={product}/>
+      {
+      product ? ( <ProductCard product={product} />  ) : (   <div className="text-center mt-5">Loading product details...</div>  )
+      }
     </>
-  )
+  );
 }
 
-export default SingleProduct
+export default SingleProduct;
